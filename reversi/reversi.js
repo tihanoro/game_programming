b1878559cdf2c9a94a26125355c38e5adfd85fc4
tazawa <syutotzw@icloud.com>
2021-02-17 00:49:59 +0900

    #1 盤面が押された時に石を置く機能を実装

--- reversi/reversi.js
+++ reversi/reversi.js
@@ -11,7 +11,7 @@ const WHITE = -1; // 白石
 
 const BOARD_PX_SIZE = 500;              // 盤面のサイズ
 const CELL_PX_SIZE = BOARD_PX_SIZE / 8; // マスのサイズ
-const CELL_PX_R = CELL_PX_SIZE * 0.9;   // マスの半径
+const CELL_PX_R = CELL_PX_SIZE * 0.45;   // マスの半径
 //====================================================
 
 // マスの座標を管理するクラス
@@ -65,17 +65,28 @@ class Board {
 //=========================================
 // 雑に作ったUI
 //=========================================
+
+//盤面を表示するエリアの定義
+var cs;
+var ctx;
+
 //盤面を描写する関数
 function printBoard(){
+    //盤面を表示するエリアの定義
+    cs = document.getElementById('canvas');
+    ctx = cs.getContext('2d');
+    cs.width = BOARD_PX_SIZE;
+    cs.height = BOARD_PX_SIZE;
+
     //背景の設定
     ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, 800, 800);
+    ctx.fillRect(0, 0, BOARD_PX_SIZE, BOARD_PX_SIZE);
 
     //マスの設定
-    ctx.strokeRect(0, 0, 800, 800);
-    for(i=0;i<800;i+=100){
-        for(j=0;j<800;j+=100){
-            ctx.strokeRect(i, j, 100, 100);
+    ctx.strokeRect(0, 0, BOARD_PX_SIZE, BOARD_PX_SIZE);
+    for(i=0;i<BOARD_PX_SIZE;i+=CELL_PX_SIZE){
+        for(j=0;j<BOARD_PX_SIZE;j+=CELL_PX_SIZE){
+            ctx.strokeRect(i, j, CELL_PX_SIZE, CELL_PX_SIZE);
         }
     }
     
@@ -90,42 +101,45 @@ function remove(){
     ctx.clearRect(0, 0, cs.width, cs.height);
 }
 
-//セルを書く関数
+//黒色のセルを書く関数
 function writeBlackCell(y, x){
     console.log("get:"+y+","+x);
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.beginPath();
-    ctx.arc(y*100 + 50, x*100 + 50, 45, 0, Math.PI*2, true);
+    ctx.arc(y*CELL_PX_SIZE + (CELL_PX_SIZE/2), x*CELL_PX_SIZE + (CELL_PX_SIZE/2), CELL_PX_R, 0, Math.PI*2, true);
     ctx.fill();
     console.log("put black cell");
 }
 
+//白色のセルを書く関数
 function writeWhiteCell(y, x){
+    console.log("get:"+y+","+x);
     ctx.fillStyle = "rgb(255,255,255)";
     ctx.beginPath();
-    ctx.arc(y*100 + 50, x*100 + 50, 45, 0, Math.PI*2, true);
+    ctx.arc(y*CELL_PX_SIZE + (CELL_PX_SIZE/2), x*CELL_PX_SIZE + (CELL_PX_SIZE/2), CELL_PX_R, 0, Math.PI*2, true);
     ctx.fill();
     console.log("put white cell");
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    //指定されたラジオボタンの値を取得
-    function getRadioValue(name){
-        var result = "";
-        var elems = document.getElementsByName(name);
+//指定されたラジオボタンの値を取得
+function getRadioValue(name){
+    var result = "";
+    var elems = document.getElementsByName(name);
 
-        //ラジオボタンを走査し、チェック状態にあるかを確認
-        for(var i=0, len = elems.length; i < len; i++){
-            var elem = elems.item(i);
-            //チェックされている項目の値を配列に追加
-            if(elem.checked){
-                result = elem.value;
-                break;
-            }
+    //ラジオボタンを走査し、チェック状態にあるかを確認
+    for(var i=0, len = elems.length; i < len; i++){
+        var elem = elems.item(i);
+        //チェックされている項目の値を配列に追加
+        if(elem.checked){
+            result = elem.value;
+            break;
         }
-        return result;
     }
-    
+    return result;
+}
+
+//ボタンが押された時にセルを配置する関数
+document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("btn").addEventListener("click", function() {
         var dataY = document.getElementById("y");
         var dataX = document.getElementById("x");
@@ -138,29 +152,25 @@ document.addEventListener("DOMContentLoaded", function(){
     }, false);
 }, false);
 
+//盤面をクリックされた時にセルを配置する関数
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("canvas").addEventListener("click", function(e) {
-        var dataY = e.clientX;
-        var dataX = e.clientY;
-        if(e.button === 0){
-            writeBlackCell(dataY.value, dataX.value);
-        }else if(e.button === 2){
-            writeWhiteCell(dataY.value, dataX.value);
+        //canvas要素の始点の値を取得
+        var rect = e.target.getBoundingClientRect();
+
+        //canvas上でのマウスの座標を取得
+        var mouseY = e.clientX - rect.left;
+        var mouseX = e.clientY - rect.top;
+
+        //セルの位置の取得
+        var dataX = Math.floor(mouseX/CELL_PX_SIZE);
+        var dataY = Math.floor(mouseY/CELL_PX_SIZE);
+        var color = getRadioValue("color");
+
+        if(color == "black"){
+            writeBlackCell(dataY, dataX);
+        }else{
+            writeWhiteCell(dataY, dataX);
         }
     }, false);
 }, false);
-
-var mouseX;
-var mouseY;
-/*
-cs.onclick = function(e) {
-    var rect = e.target.getBoundingClientRect();
-    mouseX = Math.floor(e.clientX - Math.floor(rect.left)/100);
-    mouseY = Math.floor(e.clientX - Math.floor(rect.top)/100);
-    if(e.button === 0){
-        writeBlackCell(mouseX,mouseY);
-    }else if(e.button === 2){
-        writeWhiteCell(mouseX,mouseY);
-    }
-}
-*/
